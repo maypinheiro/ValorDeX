79fd02f0aad9f7e140d6c350a407afdca547c7ee
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-09-09 15:52:22 -0300

    Adicionar arquivos de projeto.

---
+++ ValorX/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ValorX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = Console.ReadLine();
+            char[] valor = conta.ToCharArray();
+
+            if (Char.IsDigit(valor[0]) == false)
+            {
+                var calculo = int.Parse(valor[4].ToString()) - int.Parse(valor[2].ToString());
+                Console.WriteLine(calculo);
+            }
+            else if (Char.IsDigit(valor[2]) == false)
+            {
+                var calculo =int.Parse(valor[0].ToString()) - int.Parse(valor[4].ToString());
+                Console.WriteLine(calculo);
+            }
+            else if (Char.IsDigit(valor[4]) == false)
+            {
+                var calculo = int.Parse(valor[0].ToString()) + int.Parse(valor[2].ToString());
+                Console.WriteLine(calculo);
+            }
+            else
+            {
+                Console.WriteLine("Teste");
+            }
+
+        }
+    }
+}
+
